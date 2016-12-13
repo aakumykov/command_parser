aacc6b9f8d43425e26c326f3e2cdb8882ca1e02b
Андрей Кумыков <aakumykov@yandex.ru>
2016-12-13 21:19:16 +0700

    parse_command --> command_parser

--- command_parser.ino
+++ command_parser.ino
@@ -1,8 +1,7 @@
-void setup() {
-  Serial.begin(9600); while(!Serial);
 
-  char str[] = "command;data;3245234;23525;";
-  char delimiter[] = ";";
+String* qwerty(char* str, char* delimiter) {
+  //char str[] = "command;data;3245234;23525;";
+  //char delimiter[] = ";";
   
   char* pch;
   String cmd[50];
@@ -16,10 +15,21 @@ void setup() {
     pch = strtok (NULL, delimiter);
   }
 
+  String cmd2[cmd_index];
   Serial.println("cmd_index: "+String(cmd_index));
+  
   for (int i=0; i<cmd_index; i++) {
+    cmd2[i] = cmd[i];
     Serial.println(String(i)+": "+String(cmd[i]));
   }
+  
+  return cmd2;
+}
+
+void setup() {
+  Serial.begin(9600); while(!Serial);
+
+  String data[] = qwerty("command;data;3245234;23525;", ";");
 }
 
 void loop() {
